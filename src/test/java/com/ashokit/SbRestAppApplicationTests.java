bfd4cfdc07572e47a3cc51a2d20e742b1b3a6ed4
OM SANKATA MOCHANA PANDA <pandaomsankatamochana123@gmail.com>
2025-07-29 07:30:11 +0530

    SbRestAppApplicationTests.java

--- src/test/java/com/ashokit/SbRestAppApplicationTests.java
+++ src/test/java/com/ashokit/SbRestAppApplicationTests.java
@@ -3,6 +3,10 @@ package com.ashokit;
 class SbRestAppApplicationTests {
 
 	void contextLoads() {
+		int a=50;
+		int b=60;
+		int sum=a+b;
+		System.out.println("sum of a and b is " + sum);
 	}
 
 }
